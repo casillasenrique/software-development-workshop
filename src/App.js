48f493e2e74838fb0018ba549ff10193a41bc849
Enrique <enriquec@mit.edu>
2022-11-08 20:10:03 -0500

    Added a counter state in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
